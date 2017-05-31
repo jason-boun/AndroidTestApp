36b90c752e50af739e8ae61d71a8052fae1f3837
jason-boun <jasonboun@163.com>
2017-05-31 18:38:59 +0800

    shell sort code

--- app/src/main/java/com/jason/test/testapp/java/dss/SortUtil.java
+++ app/src/main/java/com/jason/test/testapp/java/dss/SortUtil.java
@@ -13,12 +13,14 @@ public class SortUtil {
 //        printArr(testArr);
 //        bubbleSort(testArr);
 //        selectSort(testArr);
-        insertSort(testArr);
+//        insertSort(testArr);
+//        mergeSort(testArr);
+        shellSort(testArr);
         printArr(testArr);
     }
 
     /**
-     * 冒泡排序
+     * 冒泡排序，时间复杂度O(N^2)
      *
      * @param a
      */
@@ -37,7 +39,7 @@ public class SortUtil {
     }
 
     /**
-     * 选择排序
+     * 选择排序，时间复杂度O(N^2)
      *
      * @param a
      */
@@ -58,7 +60,7 @@ public class SortUtil {
     }
 
     /**
-     * 插入排序
+     * 插入排序，时间复杂度O(N^2)
      *
      * @param a
      */
@@ -75,13 +77,89 @@ public class SortUtil {
                 index = in;
             }
             a[index] = temp;
+        }
+    }
 
-//            in = out;
-//            while (in > 0 && a[in - 1] > temp) {
-//                a[in] = a[in - 1];
-//                --in;
-//            }
-//            a[in] = temp;
+    /**
+     * 归并排序，时间复杂度O(N*logN)
+     *
+     * @param a
+     */
+    public static void mergeSort(int[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(int[] a, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        sort(a, left, mid);
+        sort(a, mid + 1, right);
+        merge(a, left, mid, right);
+    }
+
+    public static void merge(int[] a, int left, int mid, int right) {
+        int aIndex = left;
+        int[] temp = new int[a.length];
+        int tempIndex = left;
+        int rightIndex = mid + 1;
+
+        // 逐个归并：归并左右两个数组相同长度的部分
+        while (left <= mid && rightIndex <= right) {
+            if (a[left] <= a[rightIndex]) {
+                temp[tempIndex++] = a[left++];
+            } else {
+                temp[tempIndex++] = a[rightIndex++];
+            }
+        }
+        //将左边剩余的归并
+        while (left <= mid) {
+            temp[tempIndex++] = a[left++];
+        }
+        //将右边剩余的归并
+        while (rightIndex <= right) {
+            temp[tempIndex++] = a[rightIndex++];
+        }
+        //从临时数组拷贝到原数组
+        while (aIndex <= right) {
+            a[aIndex] = temp[aIndex];
+            aIndex++;
+        }
+    }
+
+    /**
+     * 希尔排序，时间复杂度O(N*(logN)^2)
+     *
+     * @param a
+     */
+    public static void shellSort(int[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        //计算增量h（从最大开始）
+        int h = 1;
+        while (h < a.length / 3) {
+            h = h * 3 + 1;
+        }
+        //开始希尔排序
+        int out, in, temp;
+        while (h > 0) {
+            for (out = h; out < a.length; out++) {
+                //零时变量记录
+                temp = a[out];
+                in = out;
+                while (in > h - 1 && a[in - h] >= temp) {
+                    a[in] = a[in - h];
+                    in = in - h;
+                }
+                a[in] = temp;
+            }
+            //增量依次递减
+            h = (h - 1) / 3;
         }
     }
 
@@ -99,7 +177,7 @@ public class SortUtil {
             return;
         }
         for (int i : a) {
-            LogUtil.printSystemInfo(i + "");
+            LogUtil.printSystemInfo("arr", i + "");
         }
     }
 }
